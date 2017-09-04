4026c8fc6a942f5ca112fac0f9c1b48f4529f2c8
Bikash Pandey <pandeybikash98@gmail.com>
2017-09-05 00:01:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New HTML';
@@ -15,3 +15,13 @@ madi.onclick=function(){
    var interval = setInterval(moveRight, 50); 
   // madi.style.marginLeft='100px';
 }
+*/
+var count=document.getElementById("counter");
+button.onclick = function(){
+    //make a request to counter end point 
+    //capture the response and store it in a variable
+    //render that variable in the correct span
+    counter=counter + 1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
+};
