c11e34a2c62999308558c1f94ceb5ab4aabc10e6
Bikash Pandey <pandeybikash98@gmail.com>
2017-09-05 00:35:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,12 +17,31 @@ madi.onclick=function(){
 }
 */
 var button=document.getElementById("counter");
-var counter=0;
+//var counter=0;
 button.onclick = function(){
-    //make a request to counter end point 
+    //Create a request object to counter end point 
+    var request = new XMLHttpRequest();
     //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+                
+            }
+        }
+        //Don't do anything
+    };
     //render that variable in the correct span
-    counter = counter + 1;
+    
+    /*counter = counter + 1;
     var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
+    span.innerHTML=counter.toString();*/
+    
+    //MAKE THE REQUEST
+    
+    request.open('GET','http://pandeybikash98.imad.hasura-app.io/counter',true);
+    request.send('null');
 };
