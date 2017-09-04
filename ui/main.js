1e348819afae5e0140801be157b41af67f19a8e9
Bikash Pandey <pandeybikash98@gmail.com>
2017-09-05 01:08:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,3 +45,33 @@ button.onclick = function(){
     request.open('GET','http://pandeybikash98.imad.hasura-app.io/counter',true);
     request.send('null');
 };
+
+//Submit Name
+var nameInput=document.getElementById("name");
+var name = nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+//shhould make a request to the server and send the name
+
+//capture the name and render it as a list.
+var names = ["name1","name2","name3","name4"];
+for (var i=0;i<=names.length;i++)
+{
+    list+='<li>'+names[i]+'</li>';
+}
+var ul=document.getElementById("namelist")
+ul.innerHTML=list;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
