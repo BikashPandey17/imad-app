28b940648af906661948f0d47f44af6eeb02c69f
Bikash Pandey <pandeybikash98@gmail.com>
2017-09-05 00:07:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ madi.onclick=function(){
   // madi.style.marginLeft='100px';
 }
 */
-var count=document.getElementById("counter");
+var button=document.getElementById("counter");
 var counter=0;
 button.onclick = function(){
     //make a request to counter end point 
