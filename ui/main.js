1f1678323672bf310bdfdef3503df7d8d6ad2889
Bikash Pandey <pandeybikash98@gmail.com>
2017-09-04 19:28:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML = 'New HTML';
 //move the image
 var element = document.getElementById('madi');
 madi.onclick=function(){
-    madi.style.marginLeft='100px';
+   img.style.marginLeft='100px';
 }
