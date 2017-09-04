a1f8c00e35b8582e7647b55f3b65171a1ede5e0f
Bikash Pandey <pandeybikash98@gmail.com>
2017-09-05 01:10:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -55,11 +55,11 @@ submit.onclick=function(){
 
 //capture the name and render it as a list.
 var names = ["name1","name2","name3","name4"];
-for (var i=0;i<=names.length;i++)
+for (var i=0;i<names.length;i++)
 {
     list+='<li>'+names[i]+'</li>';
 }
-var ul=document.getElementById("namelist")
+var ul=document.getElementById("namelist");
 ul.innerHTML=list;
 };
 
