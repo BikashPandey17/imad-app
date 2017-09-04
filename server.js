cc6c4ba2512e7e214dd649362f0e3e0e205bb9a3
Bikash Pandey <pandeybikash98@gmail.com>
2017-09-04 19:03:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/style2.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style2.css'));
 });
